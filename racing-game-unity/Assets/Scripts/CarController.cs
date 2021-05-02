290feec954713979e28516884fc085f020d0d4c6
Jonathan Schweizer <jonathanschweizer1997@gmail.com>
2021-05-02 18:59:18 +0200

    Car smoke animation and car prefab

--- racing-game-unity/Assets/Scripts/CarController.cs
+++ racing-game-unity/Assets/Scripts/CarController.cs
@@ -14,21 +14,30 @@ public class CarController : MonoBehaviour
     [SerializeField]
     public float maxSpeed = 100f;
 
+    [SerializeField]
+    public Animator smokeAnimator;
+
     private Vector2 currentSpeed;
 
     private Rigidbody2D rigidbody2d;
-    private Animator animator;
+    private Animator carAnimator;
 
     private void Start()
     {
         this.rigidbody2d = GetComponent<Rigidbody2D>();
-        this.animator = GetComponent<Animator>();
+        this.carAnimator = GetComponent<Animator>();
     }
 
     private void FixedUpdate() {
         this.currentSpeed = new Vector2(this.rigidbody2d.velocity.x, this.rigidbody2d.velocity.y);
 
         // Debug.Log(transform.up);
+
+        if (this.currentSpeed.magnitude > 0.1) {
+            this.smokeAnimator.Play("car_smoke");
+        } else {
+            this.smokeAnimator.Play("car_smoke_idle");
+        }
 
         // clamp to max speed
         if (this.currentSpeed.magnitude > this.maxSpeed) {
@@ -40,26 +49,26 @@ public class CarController : MonoBehaviour
             this.rigidbody2d.AddForce(transform.up * this.accelerationPower);
             this.rigidbody2d.drag = this.friction;
 
-            this.animator.Play("car_idle");
+            this.carAnimator.Play("car_idle");
         }
 
         if (Input.GetKey(KeyCode.S)) {
             this.rigidbody2d.AddForce(-transform.up * this.accelerationPower);
             this.rigidbody2d.drag = this.friction;
 
-            this.animator.Play("car_idle");
+            this.carAnimator.Play("car_idle");
         }
 
         if (Input.GetKey(KeyCode.A) && this.currentSpeed.magnitude > 0.1) {
             transform.Rotate(Vector3.forward * this.steeringPower);
 
-            this.animator.Play("car_left");
+            this.carAnimator.Play("car_left");
         }
 
         if (Input.GetKey(KeyCode.D) && this.currentSpeed.magnitude > 0.1) {
             transform.Rotate(-Vector3.forward * this.steeringPower);       
                         
-            this.animator.Play("car_right");
+            this.carAnimator.Play("car_right");
         }
     }
 }
